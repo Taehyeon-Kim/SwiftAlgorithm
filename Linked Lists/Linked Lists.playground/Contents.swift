9177a0718f0dfe06541f6c87fbdc0a7fc924b16e
김태현 <taehyk528@gmail.com>
2023-01-05 18:05:19 +0900

    Implement :: Linked List (Insertion ~ Pop)
    - push
    - append
    - insert(after:)
    - find node
    - pop(head firstly pop)
    - defer method

--- Linked Lists/Linked Lists.playground/Contents.swift
+++ Linked Lists/Linked Lists.playground/Contents.swift
@@ -1,5 +1,3 @@
-import UIKit
-
 example(of: "Linking node") {
   let node1 = Node(value: 1)
   let node2 = Node(value: 2)
@@ -9,4 +7,46 @@ example(of: "Linking node") {
   node2.next = node3
   
   print(node1)
+}
+
+example(of: "Push") {
+  var list = LinkedList<Int>()
+  list.push(3)
+  list.push(2)
+  list.push(1)
+  print(list)
+}
+
+example(of: "Append") {
+  var list = LinkedList<Double>()
+  list.append(1.0)
+  list.append(2.0)
+  list.append(3.0)
+  print(list)
+}
+
+example(of: "Insert") {
+  var list = LinkedList<Int>()
+  list.push(3)
+  list.push(2)
+  list.push(1)
+  
+  print("Before inserting: \(list)")
+  var middleNode = list.node(at: 1)!
+  for _ in stride(from: 1, through: 4, by: 1) {
+    middleNode = list.insert(-1, after: middleNode)
+  }
+  print("After inserting: \(list)")
+}
+
+example(of: "pop") {
+  var list = LinkedList<Int>()
+  list.push(3)
+  list.push(2)
+  list.push(1)
+
+  print("Before popping list: \(list)")
+  let poppedValue = list.pop()
+  print("After popping list: \(list)")
+  print("Popped value: " + String(describing: poppedValue))
 }
--- Linked Lists/Linked Lists.playground/Sources/Linked Lists.swift
+++ Linked Lists/Linked Lists.playground/Sources/Linked Lists.swift
@@ -10,6 +10,71 @@ public struct LinkedList<Value> {
   public var isEmpty: Bool {
     head == nil
   }
+  
+  // push - head firstly insertion
+  public mutating func push(_ value: Value) {
+    head = Node(value: value, next: head)
+    if tail == nil {
+      tail = head
+    }
+  }
+  
+  // append - tail end insertion
+  public mutating func append(_ value: Value) {
+    guard !isEmpty else {
+      push(value) // head, tail
+      return
+    }
+    
+    // Force unwrapping 반드시 성공함을 보장 (최초 tail에 값을 넣어줬기 때문)
+    tail!.next = Node(value: value)
+    tail = tail!.next
+  }
+  
+  // insert(after:node)
+  // 1. find node
+  // 2. insert node
+  
+  /// 1. find node
+  public func node(at index: Int) -> Node<Value>? {
+    var currentNode = head
+    var currentIndex = 0
+    
+    while currentNode != nil && currentIndex < index {
+      currentNode = currentNode!.next
+      currentIndex += 1
+    }
+    
+    return currentNode
+  }
+  
+  @discardableResult
+  public mutating func insert(_ value: Value, after node: Node<Value>) -> Node<Value> {
+    
+    /// 참조 비교 연산자 사용
+    /// 동일한 객체 인스턴스를 참조하는지 확인, insert하려는 노드가 마지막 노드(tail)인지 확인
+    guard tail !== node else {
+      append(value)
+      return tail!
+    }
+    
+    node.next = Node(value, after: node.next)
+    return node.next!
+  }
+  
+  @discardableResult
+  public mutating func pop() -> Value? {
+    // 2. head pointer는 나중에 미룬다는 느낌
+    defer {
+      head = head.next!
+      if isEmpty {
+        tail = nil
+      }
+    }
+    
+    // 1. 먼저 return 하고
+    return head?.value
+  }
 }
 
 extension LinkedList: CustomStringConvertible {
