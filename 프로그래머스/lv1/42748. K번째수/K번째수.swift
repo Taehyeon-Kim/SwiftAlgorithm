db5a0f8a231d7ad11358e13450cf953d8843400d
Taehyeon Kim <taehyk528@gmail.com>
2023-01-09 15:48:07 +0900

    [level 1] Title: K번째수, Time: 0.11 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.swift
+++ 프로그래머스/lv1/42748. K번째수/K번째수.swift
@@ -1,13 +1,8 @@
 import Foundation
 
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-    // 분할
-    // 1. 슬라이싱
-    // - Swift에서의 Slicing
-    // 2. 정렬
-    // 3. peek
-    return commands.map {
-        let i = $0[0] - 1, j = $0[1] - 1, k = $0[2] - 1
-        return array[i...j].sorted()[k]
+    
+    return commands.map { cmd in
+        return array[cmd[0]-1...cmd[1]-1].sorted()[cmd[2]-1]
     }
 }
