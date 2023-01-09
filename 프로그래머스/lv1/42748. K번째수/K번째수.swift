2868a4111d60fab2c6c8897c18629a2da49ef703
Taehyeon Kim <taehyk528@gmail.com>
2023-01-09 18:11:50 +0900

    Update new solution of K번째수

--- 프로그래머스/lv1/42748. K번째수/K번째수.swift
+++ 프로그래머스/lv1/42748. K번째수/K번째수.swift
@@ -6,3 +6,15 @@ func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
         return array[cmd[0]-1...cmd[1]-1].sorted()[cmd[2]-1]
     }
 }
+
+/* swift
+// 분할
+// 1. 슬라이싱
+// - Swift에서의 Slicing
+// 2. 정렬
+// 3. peek
+return commands.map {
+    let i = $0[0] - 1, j = $0[1] - 1, k = $0[2] - 1
+    return array[i...j].sorted()[k]
+}
+/*
