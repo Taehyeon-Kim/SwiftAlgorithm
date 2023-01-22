e09becef19cf3c7d7dc95177fe56566404f88dec
Taehyeon Kim <taehyk528@gmail.com>
2023-01-22 14:22:56 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.11 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
@@ -1,19 +1,5 @@
-
-// 이진수 계산
-func asBinary(_ x: Int, n: Int) -> String {
-    let string = String(x, radix: 2).map { $0 == "1" ? "#" : " "}
-    let len = string.count
-    return (string.count < n ? (0..<n - string.count).map { _ in " " } + string : string).joined()
-}
-
-// 합성
-func compose(_ l: String, _ r: String) -> String {
-    return zip(l, r).map { $0 == "#" || $1 == "#" ? "#" :  " " }.joined()
-}
-
 func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {    
-    let arr1 = arr1.map { asBinary($0, n: n) }
-    let arr2 = arr2.map { asBinary($0, n: n) }
-
-    return zip(arr1, arr2).map { compose($0, $1) }
+    return zip(arr1, arr2)
+    .map { String($0|$1, radix: 2).map { $0 == "1" ? "#" : " " } }
+    .map { String(repeating: " ", count: n - $0.count) + $0 }
 }
