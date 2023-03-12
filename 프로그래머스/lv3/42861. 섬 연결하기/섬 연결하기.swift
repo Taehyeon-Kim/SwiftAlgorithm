75febd3d399a7138cc9ca71aa6aed70fdc193898
Taehyeon Kim <taehyk528@gmail.com>
2023-03-12 22:53:23 +0900

    [level 3] Title: 섬 연결하기, Time: 0.12 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.swift
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.swift
@@ -1,5 +1,44 @@
 import Foundation
 
 func solution(_ n:Int, _ costs:[[Int]]) -> Int {
-    return 0
+    var tuple = costs
+    tuple.sort(by: { $0[2] < $1[2] })
+    
+    var p = [Int]()
+    for i in 0..<n+1 {
+        p.append(i)
+    }
+    
+    func findParent(_ v: Int) -> Int {
+        if p[v] == v { return v }
+        p[v] = findParent(p[v])
+        return p[v]
+    }
+    
+    func union(_ u: Int, _ v: Int) -> Bool {
+        let u = findParent(u)
+        let v = findParent(v)
+        
+        if u == v { return false }
+        else { p[v] = u }
+        return true
+    }
+    
+    func kruskcal() -> Int {
+      var (ret, edges) = (0, 0)
+
+      for value in tuple {
+        var (u, v, cost) = (value[0], value[1], value[2])
+        if union(u, v) {
+          ret += cost
+          edges += 1
+          if edges == n - 1 {
+            return ret
+          }
+        }
+      }
+      return -1
+    }
+
+    return kruskcal()
 }
