ec7896aa096511db88bb9d2a6036beca53e7241c
Taehyeon Kim <taehyk528@gmail.com>
2023-03-23 14:05:46 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 220 ms, Memory: 73820 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.swift
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.swift
@@ -1,52 +1,50 @@
-func input() -> (p: [Int], tuple: [(Int, Int, Int)], v: Int, e: Int) {
-    let ve = readLine()!.split(separator: " ").compactMap {Int(String($0))}
-    let (v, e) = (ve[0], ve[1])
-    var tuple = [(Int, Int, Int)]()
-    for _ in 0..<e {
-        let line = readLine()!.split(separator: " ").compactMap {Int(String($0))}
-        let (a, b, c) = (line[0], line[1], line[2])
-        tuple.append((c, a, b))
-    }
-    tuple = tuple.sorted(by: { $0.0 < $1.0 })
-  
-    var p = [Int]()
-    for i in 0..<v+1 {
-        p.append(i)
-    }
-    
-    return (p, tuple, v, e)
+func input() -> (v: Int, e: Int, tuple: [(Int, Int, Int)]) {
+  let ve = readLine()!.split(separator: " ").compactMap{Int(String($0))}
+
+  var tuple = [(Int, Int, Int)]()
+  for _ in 0..<ve[1] {
+    let line = readLine()!.split(separator: " ").compactMap{Int(String($0))}
+    let (cost, a, b) = (line[2], line[0], line[1])
+    tuple.append((cost, a, b))
+  }
+  tuple.sort(by:{$0.0 < $1.0})
+  return (ve[0], ve[1], tuple)
 }
 
-func kruskcal() -> Int {
-    var (p, tuple, V, _) = input()
+func kruskal() -> Int {
+  let (v, e, tuple) = input()
+  var p = [Int](repeating: 0, count: v+1)
+  for i in 1..<v+1 {
+    p[i] = i
+  }
 
-    func findParent(_ x: Int) -> Int {
-        if p[x] == x { return x }
-        p[x] = findParent(p[x])
-        return p[x]
+  func findParent(_ x: Int) -> Int {
+    if p[x] == x { return x }
+    p[x] = findParent(p[x])
+    return p[x]
+  }
+
+  func union(_ u: Int, _ v: Int) -> Bool {
+    let u = findParent(u)
+    let v = findParent(v)
+    if u == v { return false }
+    if u < v { p[v] = u }
+    else { p[u] = v }
+    return true
+  }
+
+  var (cnt, costs) = (0, 0)
+  for (cost, a, b) in tuple {
+    if union(a, b) {
+      cnt += 1
+      costs += cost
+      if cnt == v-1  {
+        return costs
+      }
     }
-    
-    func union(_ u: Int, _ v: Int) -> Bool {
-        let u = findParent(u)
-        let v = findParent(v)
-        
-        if u == v { return false }
-        if u < v { p[v] = u }
-        else { p[u] = v }
-        return true
-    }
-    
-    var (ans, cnt) = (0, 0)
-    for (cost, u, v) in tuple {
-        if union(u, v) {
-            cnt += 1
-            ans += cost
-            if cnt == V - 1 {
-                return ans
-            }
-        }
-    }
-    return ans
+  }
+
+  return -1
 }
 
-print(kruskcal())
+print(kruskal())
