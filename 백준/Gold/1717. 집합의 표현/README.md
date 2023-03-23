f0c06c5d59b06f756284a7d6ca54cceb7e69987b
Taehyeon Kim <taehyk528@gmail.com>
2023-03-23 18:54:09 +0900

    [Gold V] Title: 집합의 표현, Time: 228 ms, Memory: 81992 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/README.md
+++ 백준/Gold/1717. 집합의 표현/README.md
@@ -1,14 +1,14 @@
-# [Gold IV] 집합의 표현 - 1717 
+# [Gold V] 집합의 표현 - 1717 
 
 [문제 링크](https://www.acmicpc.net/problem/1717) 
 
 ### 성능 요약
 
-메모리: 81992 KB, 시간: 232 ms
+메모리: 81992 KB, 시간: 228 ms
 
 ### 분류
 
-자료 구조(data_structures), 분리 집합(disjoint_set)
+자료 구조, 분리 집합
 
 ### 문제 설명
 
--- 백준/Gold/1717. 집합의 표현/집합의 표현.swift
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.swift
@@ -6,14 +6,12 @@ for i in 0..<n+1 {
     p.append(i)
 }
 
-/// 부모 노드 찾는 메서드
 func findParent(_ x: Int) -> Int {
     if p[x] == x { return x }
     p[x] = findParent(p[x])
     return p[x]
 }
 
-/// 합치는 메서드
 func union(_ u: Int, _ v: Int) {
     let u = findParent(u)
     let v = findParent(v)
@@ -22,8 +20,7 @@ func union(_ u: Int, _ v: Int) {
     else { p[u] = v }
 }
 
-/// 같은 부모를 가리키는지 즉, 같은 집합인지 체크하는 메서드
-func sameParent(_ u: Int, _ v: Int) -> Bool {
+func isSame(_ u: Int, _ v: Int) -> Bool {
     let u = findParent(u)
     let v = findParent(v)
     if u == v { return true }
@@ -37,6 +34,6 @@ for _ in 0..<m {
     if cmd == 0 {
         union(a, b)
     } else {
-        print(sameParent(a, b) ? "YES" : "NO")
+        print(isSame(a, b) ? "YES" : "NO")
     }
 }
