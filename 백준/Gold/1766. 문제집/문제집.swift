becff58cba80bf49b3428269840aa6d254a87144
Taehyeon Kim <taehyk528@gmail.com>
2023-03-28 17:28:40 +0900

    [Gold II] Title: 문제집, Time: 816 ms, Memory: 74056 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.swift
@@ -0,0 +1,46 @@
+let nm = readLine()!.split(separator: " ").compactMap{Int(String($0))}
+let (n,m) = (nm[0], nm[1])
+
+var adj: [Int: [Int]] = [:]
+for _ in 0..<m {
+    let ab = readLine()!.split(separator: " ").compactMap{Int(String($0))}
+    adj[ab[0], default: []].append(ab[1])
+}
+
+var indegrees = [Int](repeating: 0, count: n+1)
+for i in indegrees.indices {
+    if let values = adj[i] {
+        for j in values {
+            indegrees[j] += 1
+        }
+    }
+}
+
+var q = [Int]()
+var idx = 0
+
+for (idx, value) in indegrees.enumerated() {
+    if value == 0 && idx != 0 {
+        q.append(idx)
+    }
+}
+
+q.sort()
+
+var ret = [Int]()
+while !q.isEmpty {
+    let v = q.removeFirst()
+    ret.append(v)
+
+    if let values = adj[v] {
+        for i in values {
+            indegrees[i] -= 1
+            if indegrees[i] == 0 {
+                q.append(i)
+                q.sort()
+            }
+        }
+    }
+}
+
+print(ret.map{String($0)}.joined(separator: " "))
