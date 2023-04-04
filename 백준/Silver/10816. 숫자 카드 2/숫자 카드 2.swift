9ae6c682bfe99bbdeae70f7a02257dfafc3beaa0
Taehyeon Kim <taehyk528@gmail.com>
2023-04-04 20:36:54 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 868 ms, Memory: 115728 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.swift
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.swift
@@ -1,48 +1,34 @@
-import Foundation
-
 let n = Int(readLine()!)!
 var cards = readLine()!.split(separator: " ").map{ Int($0)! }.sorted(by: <)
-cards.append(cards[cards.count - 1] + 1)
 let m = Int(readLine()!)!
 let nums = readLine()!.split(separator: " ").map{ Int($0)! }
 
-func lowerBound(arr: [Int], start: Int, end: Int, target: Int) -> Int {
-    var start = start
-    var end = end
-
-    while end > start {
-        let mid = start + ((end - start) / 2)
-
-        if arr[mid] >= target {
-            end = mid
-        } else {
-            start = mid + 1
-        }
-    }
-
-    return start
+func lowerBound(_ array: [Int], _ target: Int) -> Int {
+  // end point가 배열의 마지막 인덱스 + 1
+  var (s, e) = (0, n)
+  while s < e {
+    let mid = (s + e) / 2
+    if array[mid] < target { s = mid + 1 }
+    else { e = mid }
+  }
+  // 같아지면 종료
+  return s
 }
 
-func upperBound(arr: [Int], start: Int, end: Int, target: Int) -> Int {
-    var start = start
-    var end = end
-
-    while end > start {
-        let mid = start + ((end - start) / 2)
-
-        if arr[mid] > target {
-            end = mid
-        } else {
-            start = mid + 1
-        }
-    }
-
-    return end
+func upperBound(_ array: [Int], _ target: Int) -> Int {
+  var (s, e) = (0, n)
+  while s < e {
+    let mid = (s + e) / 2
+    if array[mid] <= target { s = mid + 1 }
+    else { e = mid }
+  }
+  // 같아지면 종료
+  return s
 }
 
 for i in 0..<m {
     let target = nums[i]
-    let up = upperBound(arr: cards, start: 0, end: n, target: target)
-    let low = lowerBound(arr: cards, start: 0, end: n, target: target)
+    let up = upperBound(cards, target)
+    let low = lowerBound(cards, target)
     print("\(up - low)", terminator: " ")
 }
