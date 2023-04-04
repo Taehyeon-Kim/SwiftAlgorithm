e9902459a1e451081da3b23836a02760ea8fd3d4
Taehyeon Kim <taehyk528@gmail.com>
2023-04-04 19:24:46 +0900

    [Silver IV] Title: 수 찾기, Time: 216 ms, Memory: 80588 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.swift
+++ 백준/Silver/1920. 수 찾기/수 찾기.swift
@@ -6,30 +6,6 @@ n_num.sort() // 오름차순 정렬
 let m = Int(readLine()!)! // m개
 let m_num = readLine()!.split(separator: " ").compactMap{Int(String($0))}
 
-// array: 정수 배열, target: 찾아야 하는 수
-func binarySearch(_ array: [Int], _ target: Int) -> Int {
-  var (left, right) = (0, array.count - 1)
-
-  // 반복
-  while left <= right {
-    let mid = (left + right) / 2  // 중간값 - 인덱스
-    // 찾아야 하는 수가 더 크면 오른쪽 범위로 한정
-    if array[mid] < target {
-      left = mid + 1
-    }
-    // 더 작다면 왼쪽 범위로 한정
-    else if array[mid] > target {
-      right = mid - 1 
-    }
-    else {
-      return 1
-    }
-  }
-
-  // 찾아야 하는 수가 없다면 0 반환
-  return 0
-}
-
 let sets = Set(n_num)
 for num in m_num {
   // print(binarySearch(n_num, num))
