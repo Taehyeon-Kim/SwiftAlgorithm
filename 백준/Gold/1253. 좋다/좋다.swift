1a61f56cb3544330e749cf1f2eb03348f506d092
Taehyeon Kim <taehyk528@gmail.com>
2023-04-05 21:43:22 +0900

    [Gold IV] Title: 좋다, Time: 28 ms, Memory: 69280 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.swift
+++ 백준/Gold/1253. 좋다/좋다.swift
@@ -1,35 +1,27 @@
 let n = Int(readLine()!)!
-
-var nums: [Int] = []
-nums = readLine()!.split(separator: " ").compactMap{Int(String($0))}
-nums.sort()
-
-var dict: [Int: Int] = [:]
-var sets = Set<Int>()
+let nums = readLine()!.split(separator: " ").compactMap{Int(String($0))}.sorted()
 var count = 0
 
-for i in 0..<n {
-  dict[nums[i], default: 0] += 1
-}
+func isGood(_ idx: Int, _ element: Int) -> Bool {
+  // 합의 범위를 이분탐색의 시작과 끝으로 만듦
+  var (st, en) = (0, n-1)
 
-// 0이 3개 이상일 때 포함
-if let zero = dict[0], zero >= 3 {
-  sets.insert(0)
+  while st < en {
+    let target = nums[st] + nums[en]
+    if target == element {
+      if ![st, en].contains(idx) { return true }
+      else if st == idx { st += 1 }
+      else if en == idx { en -= 1 }
+    }
+    else if target < element { st += 1 }
+    else { en -= 1 }
+  }
+  
+  return false
 }
 
 for i in stride(from: 0, to: n, by: 1) {
-  for j in stride(from: i+1, to: n, by: 1) {
-    if nums[i] == 0 && nums[j] == 0 { continue }
-    if nums[i] == 0 && dict[nums[j]] == 1 { continue }
-    if nums[j] == 0 && dict[nums[i]] == 1 { continue }
-    sets.insert(nums[i] + nums[j])
-  }
-}
-
-for i in 0..<n {
-  if sets.contains(nums[i]) {
-    count += 1
-  }
+  if isGood(i, nums[i]) { count += 1 }
 }
 
 print(count)
