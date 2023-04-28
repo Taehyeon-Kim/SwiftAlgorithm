1728cf84286cd3afb8b6b43b2b2cfec1173a0566
Taehyeon Kim <taehyk528@gmail.com>
2023-04-28 17:48:29 +0900

    [Silver I] Title: 곱셈, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.swift
+++ 백준/Silver/1629. 곱셈/곱셈.swift
@@ -1,14 +1,13 @@
-import Foundation
-let line = readLine()!.components(separatedBy: " ").map { Int($0)! }
-let a = line[0], b = line[1], c = line[2]
+func solution(_ a: Int, _ b: Int, _ m: Int) -> Int {
+  if b == 1 { return a % m }
 
-func pow(_ a: Int, _ b: Int, _ c: Int) -> Int {
-    if b == 1 { return a % c }
-    
-    var val = pow(a, b/2, c)
-    val = val * val % c
-    if b % 2 == 0 { return val }
-    return a * val % c
+  var val = solution(a, b/2, m)
+  val = val * val % m
+
+  if b % 2 == 0 { return val }
+  return val * a % m
 }
 
-print(pow(a, b, c))
+let input = readLine()!.split(separator: " ").compactMap{Int(String($0))}
+let (a, b, c) = (input[0], input[1], input[2])
+print(solution(a, b, c))
